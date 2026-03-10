fa766ebf8eccc19545d3113a9d6adb7f09534fa7
kaviyagurunathan <132463988+kaviyagurunathan@users.noreply.github.com>
2026-03-10 11:51:24 +0530

    Add files via upload

---
+++ bubblesort.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int arr[] = {5,3,8,2,1};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    for(int i=0;i<n-1;i++){
+        if(arr[i] > arr[i+1]){
+            int temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
