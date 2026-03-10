2dfbc396b0a57086cd13a29aff77a73b8ff1102c
kaviyagurunathan <132463988+kaviyagurunathan@users.noreply.github.com>
2026-03-10 09:19:52 +0530

    Add files via upload

---
+++ CeltoFaren.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int cel,faren;
+    cout << "enter celcius" << endl;
+    cin >> cel;
+
+    faren = (cel*9/5) + 32;
+
+    cout << faren << endl;
+
+    return 0;
+
+}
---
+++ print.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Program started\n";
+    std::cout << "Hello World\n";
+    std::cout << "Program finished\n";
+    return 0;
+}
