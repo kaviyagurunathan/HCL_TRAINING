9d1bd27e6e0bce1e1b8f18b88665d0acd913d3ed
kaviyagurunathan <132463988+kaviyagurunathan@users.noreply.github.com>
2026-03-11 08:47:35 +0530

    merging two arrays into single array

---
+++ HCLTRACKPLAN/mergeArr.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n1 = 3, n2 = 3;
+    int arr1[] = {1,3,5};
+    int arr2[] = {2,4,6};
+    int merged[n1+n2];
+    int i=0, j=0, k=0;
+    while(i<n1 && j<n2) {
+        if(arr1[i] < arr2[j])
+            merged[k++] = arr1[i++];
+        else
+            merged[k++] = arr2[j++];
+    }
+    while(i<n1)
+        merged[k++] = arr1[i++];
+    while(j<n2)
+        merged[k++] = arr2[j++];
+    cout << "Merged array:\n";
+    for(int x=0; x<n1+n2; x++)
+        cout << merged[x] << " ";
+}
