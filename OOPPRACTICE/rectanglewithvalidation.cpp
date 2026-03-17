4257d9f2434ab1a2ad0e437755aec6a70bdd30df
kaviyagurunathan <132463988+kaviyagurunathan@users.noreply.github.com>
2026-03-17 10:09:11 +0530

    Rectangle validation

---
+++ OOPPRACTICE/rectanglewithvalidation.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+class Rectangle {
+private:
+    double width;
+    double height;
+
+public:
+    void setWidth(double w) {
+        if (w <= 0)
+            throw invalid_argument("Width must be > 0");
+        width = w;
+    }
+
+    void setHeight(double h) {
+        if (h <= 0)
+            throw invalid_argument("Height must be > 0");
+        height = h;
+    }
+
+    double area() {
+        return width * height;
+    }
+};
+
+int main() {
+    Rectangle r;
+
+    try {
+        r.setWidth(5);
+        r.setHeight(-3);
+    } catch (exception& e) {
+        cout << "Error: " << e.what() << endl;
+    }
+
+    return 0;
+}
